8ab49b9cbf37321e524328edd6eafef96946150d
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-10-17 21:41:19 +0530

    Kaustav | #0000 | Updated gatsby configuration.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kaustav Chakraborty`,
+    description: "",
+    author: `@phoenixTW`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,17 +19,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Kaustav Chakraborty - Personal website`,
+        short_name: `Kaustav Chakraborty`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#232f38`,
+        theme_color: `#232f38`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
