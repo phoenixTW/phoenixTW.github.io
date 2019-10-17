dcf7a2002df1ff84861efe0d48171d7dc41e98b3
Kaustav Chakraborty <kaustav.ron@gmail.com>
2019-10-17 23:51:19 +0530

    Kaustav | #0000 | Added typing effect.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,29 @@
 import React from "react"
+import ReactTypingEffect from "react-typing-effect"
 
 import { Layout, SEO, Image } from "@components"
 import "@styles/pages/index.scss"
+
+const messages = [
+    "A DEVELOPER.",
+    "A CONSULTANT.",
+    "A SPEAKER.",
+    "AN OPEN SOURCE CONTRIBUTOR.",
+    "KAUSTAV CHAKRABORTY",
+]
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
       <div className="home-page__wrapper">
           <Image className="home-image__wrapper" />
+          <ReactTypingEffect
+              className="home-page__typing-effect"
+              text={messages}
+              eraseDelay={2000}
+              staticText="I'M"
+          />
+
       </div>
   </Layout>
 )
